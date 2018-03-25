cc5084754d4dec9024faf91891ac97e63714ec21
Shokaah <anton.che@core-tech.ru>
2018-03-26 00:14:40 +0300

    Admin created

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from core.views import test_index
+from core.views import main_page
 
 urlpatterns = [
-
-    url(r'^$', test_index, name="test_index")
+    url(r'^$', main_page, name='main_page'),
 ]
